90451c56199fad88ce4cf38542e6e4e9b520a329
valdal14 <valdal14@gmail.com>
2018-10-27 10:25:32 +0200

    Added the functionalities to create the project structure and related required files

--- spec/wizard-spec.js
+++ spec/wizard-spec.js
@@ -19,7 +19,9 @@ describe("Configuration Variables", () => {
   });
 
   it("Extension ID should throw an error if the length is not valid", () => {
-    expect((wizard.extensionId = "231241434")).toThrow();
+    expect(function() {
+      wizard.extensionId("231241434");
+    }).toThrow();
   });
 
   // Developer ID
@@ -31,13 +33,19 @@ describe("Configuration Variables", () => {
     expect(parseInt(wizard._developerID)).not.toBeNaN();
   });
 
+  it("Should throw if the value passed is not an integer", () => {
+    expect(function() {
+      wizard.developerID("weyuweir");
+    }).toThrow();
+  });
+
   // Created Time
   it("Date should be today", () => {
-    expect(Wizard.creationDate()).toEqual("2018-10-5");
+    expect(Wizard.creationDate()).toEqual("2018-10-6");
   });
 
   it("Date should be today", () => {
-    expect(wizard._created).toEqual("2018-10-5");
+    expect(wizard._created).toEqual("2018-10-6");
   });
 
   // Created by
@@ -45,13 +53,31 @@ describe("Configuration Variables", () => {
     expect(wizard._createdBy).toBeDefined();
   });
 
+  it("Created by should throw if if the argument length is 0", () => {
+    expect(function() {
+      wizard.createdBy();
+    }).toThrow();
+  });
+
   // Extension name
   it("Extension name should not be undefined", () => {
     expect(wizard._extensionName).toBeDefined();
   });
 
+  it("Extension name should throw if if the argument length is 0", () => {
+    expect(function() {
+      wizard.extensionName();
+    }).toThrow();
+  });
+
   // Description name
   it("Description should not be undefined", () => {
     expect(wizard._description).toBeDefined();
+  });
+
+  it("Description should throw if if the argument length is 0", () => {
+    expect(function() {
+      wizard.description();
+    }).toThrow();
   });
 });
--- wizard.js
+++ wizard.js
@@ -1,4 +1,7 @@
 "use strict";
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 
 class Wizard {
   constructor(extensionId, developerID, createdBy, extensionName, description) {
@@ -10,6 +13,7 @@ class Wizard {
     this._createdBy = createdBy;
     this._extensionName = extensionName;
     this._description = description;
+    this._path = os.homedir();
   }
 
   set extensionId(ext) {
@@ -79,6 +83,249 @@ class Wizard {
     let date = new Date();
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay()}`;
   }
+
+  /**
+   * Create the ext.json file
+   */
+  createExtJson() {
+    let jsonFile = {
+      extensionID: this._extensionId,
+      developerID: this._developerID,
+      createdBy: this._createdBy,
+      name: this._extensionName,
+      timeCreated: this._created,
+      description: this._description
+    };
+
+    return JSON.stringify(jsonFile);
+  }
+
+  /**
+   * Create the widget.json file
+   */
+  createWidgetJson() {
+    let wjsonFile = {
+      name: this._extensionName,
+      version: 1,
+      global: false,
+      javascript: "widget-javascript",
+      pageTypes: ["home"],
+      imports: ["products"],
+      jsEditable: true,
+      config: {}
+    };
+
+    return JSON.stringify(wjsonFile);
+  }
+
+  /**
+   * Create the JS file
+   */
+  createJavascriptFile() {
+    let jsFile = `
+      // Javascript widget file \n
+      // Implementation guide \n
+      // https://docs.oracle.com/cd/E65426_01/Cloud.15-3/WidgetDev/html/s0207createcustomjavascript01.html \n\r
+
+      define( \n
+        // Dependencies\n
+        ['jquery', 'knockout'],\n\r
+      
+        // Module Implementation \n
+        function(ko) { \n
+           // We recommend enabling strict checking mode \n
+           'use strict'; \n\r
+         
+           // Private variables and functions can be defined here... \n
+           var SOME_CONSTANT = 1024; \n\r
+      
+           var privateMethod = function () { \n
+             // ... \n
+           }; \n\r
+      
+           return { \n
+            // Widget JS \n
+            // Some member variables... \n
+            textInput: ko.observable(), \n
+            // Some public methods... \n
+            doSomethingWithInput: function () { \n
+              //... \n
+            } \n
+          } \n
+      }); \n\r
+
+      `;
+
+    return jsFile;
+  }
+
+  /**
+   * Create the display.template file
+   */
+  createTheHtmlTemplate() {
+    let display = `
+      // Template File \n
+      // Implementation guide: \n
+      // https://docs.oracle.com/cd/E65426_01/Cloud.15-3/WidgetDev/html/s0209createaconfigurablelayout01.html \n\r
+
+      <div class="myWidget"> /n
+      \n
+      </div> \n\r
+    `;
+
+    return display;
+  }
+
+  /**
+   * Create the widget.less file
+   */
+  createTheLessFile() {
+    let wCss = `
+    // Widget CSS Less \n
+    // Implementation guide: \n
+    // https://docs.oracle.com/cd/E65426_01/Cloud.15-3/WidgetDev/html/s0207configureawidgetsstyle01.html \n\r
+    
+    .myWidget { \n
+      \n
+    } \n\r
+    
+    `;
+
+    return wCss;
+  }
+
+  /**
+   * Create the base folder structure for the widget
+   */
+  createProjectStructure() {
+    const curDir = path.resolve(w._path);
+
+    try {
+      if (!fs.existsSync(curDir)) {
+        console.log("The widget folder already exists in you HDD");
+      } else {
+        // create the container folder
+        fs.mkdirSync(`${curDir}/${this._extensionName}`);
+
+        //create the widget folder
+        fs.mkdirSync(`${curDir}/${this._extensionName}/widget`);
+
+        // create the widget name folder
+        fs.mkdirSync(
+          `${curDir}/${this._extensionName}/widget/${this._extensionName}`
+        );
+
+        //create the js folder
+        fs.mkdirSync(
+          `${curDir}/${this._extensionName}/widget/${this._extensionName}/js`
+        );
+
+        //create the less folder
+        fs.mkdirSync(
+          `${curDir}/${this._extensionName}/widget/${this._extensionName}/less`
+        );
+
+        //create the template folder
+        fs.mkdirSync(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/templates`
+        );
+
+        //create the config folder
+        fs.mkdirSync(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/config`
+        );
+
+        // create the ext.json file
+        fs.writeFile(
+          `${curDir}/${this._extensionName}/ext.json`,
+          this.createExtJson(),
+          error => {
+            if (error) {
+              throw new Error("Cannot write file ext.json");
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+
+        //create the widget.json file
+        fs.writeFile(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/widget.json`,
+          this.createWidgetJson(),
+          error => {
+            if (error) {
+              throw new Error("Cannot write file widget.json");
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+
+        //create the javascript file
+        fs.writeFile(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/js/widget-javascript.js`,
+          this.createJavascriptFile(),
+          error => {
+            if (error) {
+              throw new Error("Cannot write file widget-javascript.js");
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+
+        //create the display.template file
+        fs.writeFile(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/templates/display.template`,
+          this.createTheHtmlTemplate(),
+          error => {
+            if (error) {
+              throw new Error("Cannot write display.template file");
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+
+        //create the widget.less file
+        fs.writeFile(
+          `${curDir}/${this._extensionName}/widget/${
+            this._extensionName
+          }/less/widget.less`,
+          this.createTheLessFile(),
+          error => {
+            if (error) {
+              throw new Error("Cannot write widget.less file");
+            } else {
+              console.log("The file was saved!");
+            }
+          }
+        );
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
+
+let w = new Wizard(
+  "8defeb5d-e958-4cb7-be7c-e28a37ec995d",
+  "12345678",
+  "Valerio D'Alessio",
+  "Example Widget",
+  "Open a service request to Oracle Service Cloud"
+);
+
+w.createProjectStructure();
 
 module.exports = Wizard;
